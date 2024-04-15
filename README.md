3c5b4317784a535a2132c3f6ea852f52762c8d6b
Raj <rajendrajhairnar49@gmail.com>
2024-04-15 23:51:35 +0530

    all basic authentication api done

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
- 
+This mini project includes BasicAuthentication, SessionAuthentication, TokenAuthentication,customAuthentication, and RemoteUserAuthentication
+
+1. Basic Authentication:-
+
+This authentication scheme uses HTTP Basic Authentication, signed against a user's username and password. Basic authentication is generally only appropriate for testing.
+
+If successfully authenticated, BasicAuthentication provides the following credentials.
+
+request.user will be a Django User instance.
+request.auth will be None.
+Unauthenticated responses that are denied permission will result in an HTTP 401 Unauthorized response with an appropriate WWW-Authenticate header. For example:WWW-Authenticate: Basic realm="api"
+Note- check the settings.py file for DEFAULT_AUTHENTICATION_CLASSES and add the authentication classes and permission classes in the class of API.
+EX- class EmployeeView(APIView):
+    authentication_classes = [BasicAuthentication,]
+    permission_classes = [IsAuthenticated,IsAdminUser]
+    permission_classes=[AllowAny]
+    permission_classes=[IsAdminUser]
+    permission_classes=[IsAuthenticatedOrReadOnly]
+    permission_classes=[DjangoModelPermissionsOrAnonReadOnly]
--- basicauthentication/views.py
+++ basicauthentication/views.py
@@ -14,7 +14,13 @@ class EmployeeView(APIView):
     authentication_classes = [BasicAuthentication,]
     permission_classes = [IsAuthenticated,IsAdminUser]
 
-    def get(self,request):
+    def get(self,request,pk=None, format=None):
+
+        if pk is not None:
+            emp = Employee.objects.get(pk=pk)
+            serializer = EmployeeSerializer(emp)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+            
         emp = Employee.objects.all()
         serializer = EmployeeSerializer(emp,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
@@ -35,3 +41,18 @@ class EmployeeView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk, format=None):
+        id = pk
+        stu = Employee.objects.get(pk=id)
+        serializer = EmployeeSerializer(stu, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Partial Data Updated'})
+        return Response(serializer.errors)
+
+    def delete(self,requet,pk,format=None):
+        
+        emp = Employee.objects.get(pk=pk)
+        emp.delete()
+        return Response({'message':'employee deleted'})
